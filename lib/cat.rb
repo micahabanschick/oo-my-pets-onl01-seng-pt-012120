466f36b68bcadb0bc29db0e3ac88e76d640c81b3
Micah Banschick <micha.ban@gmail.com>
2020-02-06 00:54:35 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,31 @@
 class Cat
   
+  attr_accessor :owner, :mood
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    self.save
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def owner=(owner) 
+    @owner = Owner.all.find{|own| own == owner}
+  end
+  
+  def name 
+    @name.dup.freeze
+  end 
+  
+  def self.all 
+    @@all.dup.freeze
+  end 
+  
   
 end
