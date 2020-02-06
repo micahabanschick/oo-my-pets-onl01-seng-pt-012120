d644dc1e56fb771b6ee9aee7f215bbde737b130b
Micah Banschick <micha.ban@gmail.com>
2020-02-06 00:53:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,4 @@
 class Dog
   
-  attr_accessor :owner, :mood
-  
-  @@all = []
-  
-  def initialize(name, owner)
-    @name = name 
-    @owner = owner
-    @mood = "nervous"
-    self.save
-  end 
-  
-  def save 
-    @@all << self 
-  end 
-  
-  def name 
-    @name.dup.freeze
-  end 
-  
-  def self.all 
-    @@all.dup.freeze
-  end 
-  
+ 
 end
