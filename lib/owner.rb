e7be058e0d4a0e2e0c8062ac876e51f10d366db6
Micah Banschick <micha.ban@gmail.com>
2020-02-06 00:28:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,10 +2,17 @@ class Owner
   
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @species = "human"
+    self.save
   end 
+  
+  def save
+    @all << self 
+  end
   
   def name 
     @name.dup.freeze
@@ -13,6 +20,22 @@ class Owner
   
   def species
     @species.dup.freeze
+  end 
+  
+  def say_species
+    self.species 
+  end 
+  
+  def self.all 
+    @@all.dup.freeze
+  end 
+  
+  def self.count
+    @@all.length 
+  end 
+  
+  def self.reset_all 
+    @@all = []
   end 
   
 end
